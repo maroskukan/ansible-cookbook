6c6b5f2a513bb044ce60e66a9e4da9a09febd6b1
Maros Kukan <maros.kukan@me.com>
2021-02-02 11:29:25 +0100

    Package updates for APT and YUM

---
+++ provision.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+OS_RELEASE=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+
+if [ "${OS_RELEASE}" = '"Ubuntu"' ]; then
+    echo "Ubuntu Detected. Starting provisioning."
+    apt-get -y update
+
+elif [ "${OS_RELEASE}" = '"CentOS Linux"' ]; then
+    echo "CentOS Detected. Starting provisioning."
+    yum -y update
+else
+    echo "Unknown OS. Provisioning skipped."
+fi
