787e05b76e7c19bdba2430ed75b07a36bb488e5c
Maros Kukan <maros.kukan@me.com>
2021-05-23 20:56:07 +0200

    Add Vagrantfile file for Focal with LXC

--- exercise/provision/provision.sh
+++ exercise/provision/provision.sh
@@ -5,6 +5,7 @@ OS_RELEASE=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
 if [ "${OS_RELEASE}" = '"Ubuntu"' ]; then
     echo "Ubuntu Detected. Starting provisioning."
     apt-get -y update
+    apt-get install -y lxc
 
 elif [ "${OS_RELEASE}" = '"CentOS Linux"' ]; then
     echo "CentOS Detected. Starting provisioning."
